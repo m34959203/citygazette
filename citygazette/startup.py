2cf4e937b2a0433a36ad58ec890351ded3b4247e
Dmitriy <your@email.com>
2025-07-22 23:56:54 +0500

    ⚙️ Добавлен startup.py с collectstatic для Render

--- citygazette/startup.py
+++ citygazette/startup.py
@@ -5,11 +5,11 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'citygazette.settings')
 
-# collectstatic при запуске
+# Выполняем collectstatic при запуске (для Render)
 try:
     from django.core.management import call_command
     call_command('collectstatic', '--noinput')
 except Exception as e:
-    print(f"❗ collectstatic error: {e}")
+    print(f"❗ Ошибка при выполнении collectstatic: {e}")
 
 application = get_wsgi_application()
